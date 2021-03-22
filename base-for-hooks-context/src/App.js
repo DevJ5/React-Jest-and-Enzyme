d8da3c0ab22a349ba2175fc98b1ed226d4681409
DevJ5 <idevelopjs@gmail.com>
2021-03-22 12:32:28 +0100

    Reducer

--- base-for-hooks-context/src/App.js
+++ base-for-hooks-context/src/App.js
@@ -1,9 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import hookActions from './actions/hookActions';
+import Input from './Input';
 import './App.css';
 
+/**
+ * reducer to update state
+ * @param {object} state - existing state
+ * @param {object} action - contains `type` and `payload` properties for the state update
+ * @returns {object} - new state
+ */
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'setSecretWord':
+      return { ...state, secretWord: action.payload };
+    default:
+      throw new Error(`Invalid action type: ${action.type}`);
+  }
+};
+
 function App() {
-  return <div data-test="component-app"></div>;
+  const [state, dispatch] = React.useReducer(reducer, { secretWord: null });
+
+  const setSecretWord = (secretWord) =>
+    dispatch({ type: 'setSecretWord', payload: secretWord });
+
+  React.useEffect(() => {
+    hookActions.getSecretWord(setSecretWord);
+  }, []);
+
+  if (!state.secretWord) {
+    return (
+      <div className="container" data-test="component-spinner">
+        <div className="spinner-border" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+        <p>Loading secret word</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container" data-test="component-app">
+      <Input secretWord={state.secretWord} />
+    </div>
+  );
 }
 
 export default App;
--- base-for-hooks-context/src/App.test.js
+++ base-for-hooks-context/src/App.test.js
@@ -7,12 +7,17 @@ import hookActions from './actions/hookActions';
 
 const mockGetSecretWord = jest.fn();
 
-const setup = () => {
+const setup = (secretWord = 'party') => {
   mockGetSecretWord.mockClear();
   hookActions.getSecretWord = mockGetSecretWord;
+
+  const mockUseReducer = jest.fn().mockReturnValue([{ secretWord }], jest.fn());
+
+  React.useReducer = mockUseReducer;
   // Enzyme doesn't run useEffect on shallow, so we use mount
   return mount(<App />);
 };
+
 test('App renders without error', () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, 'component-app');
@@ -24,5 +29,44 @@ describe('getSecretWord calls', () => {
     setup();
     // check to see if secret word was updated
     expect(mockGetSecretWord).toHaveBeenCalled();
+  });
+  test('secretWord does not update on App update', () => {
+    const wrapper = setup();
+
+    mockGetSecretWord.mockClear();
+    // To trigger update, since wrapper.update() doesn't trigger useEffect.
+    wrapper.setProps();
+
+    expect(mockGetSecretWord).not.toHaveBeenCalled();
+  });
+});
+
+describe('secretWord is not null', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup('party');
+  });
+  test('renders app when secretWord is not null', () => {
+    const component = findByTestAttr(wrapper, 'component-app');
+    expect(component.exists()).toBe(true);
+  });
+  test('does not render spinner when secretWord is not null', () => {
+    const component = findByTestAttr(wrapper, 'component-spinner');
+    expect(component.exists()).toBe(false);
+  });
+});
+
+describe('secretWord is  null', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup(null);
+  });
+  test('does not render app when secretWord is null', () => {
+    const component = findByTestAttr(wrapper, 'component-app');
+    expect(component.exists()).toBe(false);
+  });
+  test('does render spinner when secretWord is null', () => {
+    const component = findByTestAttr(wrapper, 'component-spinner');
+    expect(component.exists()).toBe(true);
   });
 });
